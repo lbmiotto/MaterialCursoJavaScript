b31c3c9d2c8c31d95a4251cceffab57b2a804695
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-10-31 18:13:26 -0300

    Exercício Analisador de Números

---
+++ Exercícios/Ex_AnalisadorNumeros/script.js
@@ -0,0 +1,62 @@
+var num = window.document.getElementById('fnum')
+var lista = window.document.getElementById('flista')
+var res = window.document.getElementById('res')
+var valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${Number(num.value)} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione um número antes!!!')
+    } else {
+        var total = valores.length
+        var menor = valores[0]
+        var maior = valores[0]
+        var soma = 0
+        var média = 0
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        média = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor número foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores é: ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${média}.</p>`
+    }
+}
